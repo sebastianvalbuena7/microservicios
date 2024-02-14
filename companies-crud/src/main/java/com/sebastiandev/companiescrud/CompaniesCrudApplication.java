4d01e6392cb2073f0fbef01f57cb7a8b7fc3fcc1
Sebastian <sebastianvalbuena25@gmail.com>
2024-02-13 19:31:54 -0500

    Agregando Servidor de Registro para Microservicios

--- companies-crud/src/main/java/com/sebastiandev/companiescrud/CompaniesCrudApplication.java
+++ companies-crud/src/main/java/com/sebastiandev/companiescrud/CompaniesCrudApplication.java
@@ -2,9 +2,11 @@ package com.sebastiandev.companiescrud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 // Documentacion de las APIS: http://localhost:8081/companies-crud/swagger-ui/index.html
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CompaniesCrudApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CompaniesCrudApplication.class, args);
---
+++ config-server/src/test/java/com/sebastiandev/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastiandev.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
