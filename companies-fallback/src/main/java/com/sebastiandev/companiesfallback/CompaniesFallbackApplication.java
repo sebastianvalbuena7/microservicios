fdf138a5ff7e8d0592447ea58aa0907dfc548a5c
Sebastian <sebastianvalbuena25@gmail.com>
2024-02-17 20:24:02 -0500

    Add Companies Fallback

---
+++ companies-fallback/src/main/java/com/sebastiandev/companiesfallback/CompaniesFallbackApplication.java
@@ -0,0 +1,13 @@
+package com.sebastiandev.companiesfallback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompaniesFallbackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompaniesFallbackApplication.class, args);
+	}
+
+}
---
+++ companies-fallback/src/test/java/com/sebastiandev/companiesfallback/CompaniesFallbackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastiandev.companiesfallback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompaniesFallbackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
